c3787537bcdf9b865053b639883cc588f2fc4253
Star Prapakamol <billyaiyai@gmail.com>
2018-03-30 00:12:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+kittens = ["Milo","Otis","Garfield"]
+
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name2){
+  kittens.unshift(name2)
+  return kittens 
+}
+
+function destructivelyRemoveLastKitten(name3){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name4){
+  kittens.shift()
+  return kittens 
+}
+
+function appendKitten(name5){
+  [...kittens,name5]
+}
